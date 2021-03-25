ceca362803d5e5399e11a53a24b82e209825b5b7
pedro <sandra@gmail.com>
2021-03-24 23:49:01 -0300

    primer commit projecto responcive entrega 1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/home.html'));
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
